e33cc9683b28bd0eaabe3d6d2097d692ed6d9532
kchia <kchia87@gmail.com>
2014-12-31 20:04:29 -0800

    Completes most basic requirements

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -1,7 +1,33 @@
 // SongQueue.js - Defines a backbone model class for the song queue.
 var SongQueue = Songs.extend({
 
-  initialize: function(){
+  initialize: function() {
+
+    this.on('dequeue', function(song){
+      this.remove(song);
+    }, this);
+
+    this.on('ended', function(song){
+      this.remove(this.at(0));
+      if(this.models.length > 0){
+        this.playFirst();
+      }
+    }, this);
+
+  },
+
+  playFirst: function() {
+    if(this.models.length > 0) {
+      console.log(this.at(0));
+      this.at(0).play();
+    }
+  },
+
+  enqueue: function(song) {
+    this.add(song);
+    if(this.models.length === 1){
+        this.playFirst();
+    }
   }
 
 });
